276d92c6a3a0aadf85e4b9e377504c849656a1cd
CP <CP@Fujitsu-BMS5>
2014-05-08 22:27:04 +0200

    changed style back to employee css for all employee jsp pages

--- src/java/commands/TransferCommand.java
+++ src/java/commands/TransferCommand.java
@@ -18,56 +18,71 @@ import java.util.logging.Logger;
 import javax.servlet.http.HttpServletRequest;
 import security.SecurityRole;
 
-public class TransferCommand extends TargetCommand
-{
+public class TransferCommand extends TargetCommand {
 
-    public TransferCommand(String target, List<SecurityRole> roles)
-    {
+    public TransferCommand(String target, List<SecurityRole> roles) {
         super(target, roles);
     }
 
-    @Override
-    public String execute(HttpServletRequest request)
-    {
+    private HttpServletRequest executeTransfer(HttpServletRequest request) {
         BankManager manager = Factory.getInstance().getManager();
-        String treg = request.getParameter("treg");
+
         AccountIdentifier sourceAccount = new AccountIdentifier(request.getParameter("source"));
-            AccountIdentifier targetAccount = new AccountIdentifier(request.getParameter("target"));
-            BigDecimal amount = new BigDecimal(request.getParameter("amount"));
-            Date trans_date = null;
-        
+        AccountIdentifier targetAccount = new AccountIdentifier(request.getParameter("target"));
+        BigDecimal amount = new BigDecimal(request.getParameter("amount"));
+        Date trans_date = null;
+        try {
+            AccountDetail aDetail = manager.transferAmount(amount, sourceAccount, targetAccount);
+            trans_date = aDetail.getTransfers().iterator().next().getDate();
+        } catch (NoSuchAccountException ex) {
+            request.setAttribute("error_message", ex);
+            Logger.getLogger(TransferCommand.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (TransferNotAcceptedException ex) {
+            Logger.getLogger(TransferCommand.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InsufficientFundsException ex) {
+            Logger.getLogger(TransferCommand.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        request.setAttribute("date", trans_date);
+        request.setAttribute("src_acc", sourceAccount.getNumber());
+        request.setAttribute("targ_acc", targetAccount.getNumber());
+        request.setAttribute("amnt", amount);
+        request.setAttribute("message", "The following transaction has been made from " + sourceAccount.getNumber()
+                + " please check details for inconsistencies");
+
+        return request;
+
+    }
+
+    @Override
+    public String execute(HttpServletRequest request) {
+
+        String treg = request.getParameter("treg");
+        HttpServletRequest requestResponse = null;
+
         if (treg.isEmpty() || treg != null) {
-            try {
-                AccountDetail aDetail = manager.transferAmount(amount, sourceAccount, targetAccount);
-                trans_date = aDetail.getTransfers().iterator().next().getDate();
-            } catch (NoSuchAccountException ex) {
-                request.setAttribute("error_message", ex);
-                Logger.getLogger(TransferCommand.class.getName()).log(Level.SEVERE, null, ex);
-            } catch (TransferNotAcceptedException ex) {
-                Logger.getLogger(TransferCommand.class.getName()).log(Level.SEVERE, null, ex);
-            } catch (InsufficientFundsException ex) {
-                Logger.getLogger(TransferCommand.class.getName()).log(Level.SEVERE, null, ex);
-            }
+            requestResponse = executeTransfer(request);
+            return super.execute(requestResponse);
 
-            request.setAttribute("date", trans_date);
-            request.setAttribute("src_acc", sourceAccount.getNumber());
-            request.setAttribute("targ_acc", targetAccount.getNumber());
-            request.setAttribute("amnt", amount);
-            request.setAttribute("message", "The following transaction has been made from " + sourceAccount.getNumber()
-                    + " please check details for inconsistencies");
-
-            return super.execute(request);
         } else {
             boolean sendt = false;
+            AccountIdentifier sourceAccount = new AccountIdentifier(request.getParameter("source"));
+            AccountIdentifier targetAccount = new AccountIdentifier(request.getParameter("target"));
+            BigDecimal amount = new BigDecimal(request.getParameter("amount"));
+
             BankRepositoryClient client = new BankRepositoryClient();
             Bank bank = client.find(treg);
             TransferRequest req = new TransferRequest(amount, sourceAccount, targetAccount);
             BankRepositoryClient restClient = new BankRepositoryClient(bank.getUrl());
             restClient.send(req);
             sendt = true;
-            return sendt+", ok";
+            if (sendt) {
+                requestResponse = executeTransfer(request);
+
+            }
+            return super.execute(requestResponse);
         }
-        
+
     }
 
 }
